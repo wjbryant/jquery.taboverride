53276d79cdb380f4f5fda17d90a6eadcd82cdb82
Bill Bryant <bill@wjbryant.com>
2013-03-05 09:53:50 -0500

    use tabs instead of spaces per jQuery style guide

--- src/jquery.taboverride.js
+++ src/jquery.taboverride.js
@@ -17,113 +17,113 @@
 
 // Use CommonJS or AMD if available
 (function ( factory ) {
-    "use strict";
+	"use strict";
 
-    if ( typeof exports === "object" && typeof require === "function" ) {
-	// Node.js/CommonJS
-	factory( require( "jquery", "taboverride" ) );
-    } else if ( typeof define === "function" && define.amd ) {
-	// AMD - Register as an anonymous module
-	// Files must be concatenated using an AMD-aware tool such as r.js
-	define( [ "jquery", "taboverride" ], factory );
-    } else {
-	// No module format - Use global variables instead
-	factory( jQuery, TABOVERRIDE );
-    }
-}(function ( $, TABOVERRIDE ) {
-    "use strict";
-
-    /**
-     * the tabOverride method - Tabs will be overridden if enable is true.
-     *
-     * @param  {Boolean} [enable=true]  whether Tab Override should be enabled
-     *                                  for the element(s)
-     * @param  {String}  [selector]     the selector string for delegated events
-     * @return {Object}                 the jQuery object
-     *
-     * @name tabOverride
-     * @memberOf jQuery.fn
-     * @function
-     * @namespace
-     */
-    var tabOverride = $.fn.tabOverride = function ( enable, selector ) {
-
-	var enablePlugin = !arguments.length || enable,
-            isDelegated = typeof selector === "string";
-
-	if ( isDelegated ) {
-            // Always remove the event handlers
-            this.off( ".tabOverride", selector );
-
-            if ( enablePlugin ) {
-                this
-                    .on( "keydown.tabOverride", selector, TABOVERRIDE.overrideKeyDown )
-                    .on( "keypress.tabOverride", selector, TABOVERRIDE.overrideKeyPress );
-            }
-        } else {
-            // The jQuery object acts as an array of elements, so it can be passed
-            // to TABOVERRIDE.set()
-            TABOVERRIDE.set( this, enablePlugin );
+	if ( typeof exports === "object" && typeof require === "function" ) {
+		// Node.js/CommonJS
+		factory( require( "jquery", "taboverride" ) );
+	} else if ( typeof define === "function" && define.amd ) {
+		// AMD - Register as an anonymous module
+		// Files must be concatenated using an AMD-aware tool such as r.js
+		define( [ "jquery", "taboverride" ], factory );
+	} else {
+		// No module format - Use global variables instead
+		factory( jQuery, TABOVERRIDE );
 	}
+}(function ( $, TABOVERRIDE ) {
+	"use strict";
 
-	// Return the jQuery object
-	return this;
-    };
+	/**
+	 * the tabOverride method - Tabs will be overridden if enable is true.
+	 *
+	 * @param  {Boolean} [enable=true]  whether Tab Override should be enabled
+	 *                                  for the element(s)
+	 * @param  {String}  [selector]     the selector string for delegated events
+	 * @return {Object}                 the jQuery object
+	 *
+	 * @name tabOverride
+	 * @memberOf jQuery.fn
+	 * @function
+	 * @namespace
+	 */
+	var tabOverride = $.fn.tabOverride = function ( enable, selector ) {
 
-    /**
-     * Gets or sets the tab size for all elements that have Tab Override enabled.
-     * 0 represents the tab character.
-     *
-     * @param  {Number}          [size]  the tab size
-     * @return {Number|Function}         the tab size or the tabOverride function
-     *
-     * @name tabSize
-     * @function
-     * @memberOf jQuery.fn.tabOverride
-     */
-    tabOverride.tabSize = TABOVERRIDE.tabSize;
+		var enablePlugin = !arguments.length || enable,
+			isDelegated = typeof selector === "string";
 
-    /**
-     * Gets or sets the auto indent setting. True if each line should be
-     * automatically indented (default = false).
-     *
-     * @param  {Boolean}          [enable]  whether auto indent should be enabled
-     * @return {Boolean|Function}           whether auto indent is enabled or the
-     *                                      tabOverride function
-     *
-     * @name autoIndent
-     * @function
-     * @memberOf jQuery.fn.tabOverride
-     */
-    tabOverride.autoIndent = TABOVERRIDE.autoIndent;
+		if ( isDelegated ) {
+			// Always remove the event handlers
+			this.off( ".tabOverride", selector );
 
-    /**
-     * Gets or sets the tab key combination.
-     *
-     * @param  {Number}        keyCode             the key code of the key to use for tab
-     * @param  {String[]}      [modifierKeyNames]  the modifier key names - valid names are
-     *                                             'alt', 'ctrl', 'meta', and 'shift'
-     * @return {String|Function}                   the current tab key combination or the
-     *                                             tabOverride function
-     *
-     * @name tabKey
-     * @function
-     * @memberOf jQuery.fn.tabOverride
-     */
-    tabOverride.tabKey = TABOVERRIDE.tabKey;
+			if ( enablePlugin ) {
+				this
+					.on( "keydown.tabOverride", selector, TABOVERRIDE.overrideKeyDown )
+					.on( "keypress.tabOverride", selector, TABOVERRIDE.overrideKeyPress );
+			}
+		} else {
+			// The jQuery object acts as an array of elements, so it can be passed
+			// to TABOVERRIDE.set()
+			TABOVERRIDE.set( this, enablePlugin );
+		}
 
-    /**
-     * Gets or sets the untab key combination.
-     *
-     * @param  {Number}        keyCode             the key code of the key to use for untab
-     * @param  {String[]}      [modifierKeyNames]  the modifier key names - valid names are
-     *                                             'alt', 'ctrl', 'meta', and 'shift'
-     * @return {String|Function}                   the current untab key combination or the
-     *                                             tabOverride function
-     *
-     * @name untabKey
-     * @function
-     * @memberOf jQuery.fn.tabOverride
-     */
-    tabOverride.untabKey = TABOVERRIDE.untabKey;
+		// Return the jQuery object
+		return this;
+	};
+
+	/**
+	 * Gets or sets the tab size for all elements that have Tab Override enabled.
+	 * 0 represents the tab character.
+	 *
+	 * @param  {Number}          [size]  the tab size
+	 * @return {Number|Function}         the tab size or the tabOverride function
+	 *
+	 * @name tabSize
+	 * @function
+	 * @memberOf jQuery.fn.tabOverride
+	 */
+	tabOverride.tabSize = TABOVERRIDE.tabSize;
+
+	/**
+	 * Gets or sets the auto indent setting. True if each line should be
+	 * automatically indented (default = false).
+	 *
+	 * @param  {Boolean}          [enable]  whether auto indent should be enabled
+	 * @return {Boolean|Function}           whether auto indent is enabled or the
+	 *                                      tabOverride function
+	 *
+	 * @name autoIndent
+	 * @function
+	 * @memberOf jQuery.fn.tabOverride
+	 */
+	tabOverride.autoIndent = TABOVERRIDE.autoIndent;
+
+	/**
+	 * Gets or sets the tab key combination.
+	 *
+	 * @param  {Number}        keyCode             the key code of the key to use for tab
+	 * @param  {String[]}      [modifierKeyNames]  the modifier key names - valid names are
+	 *                                             'alt', 'ctrl', 'meta', and 'shift'
+	 * @return {String|Function}                   the current tab key combination or the
+	 *                                             tabOverride function
+	 *
+	 * @name tabKey
+	 * @function
+	 * @memberOf jQuery.fn.tabOverride
+	 */
+	tabOverride.tabKey = TABOVERRIDE.tabKey;
+
+	/**
+	 * Gets or sets the untab key combination.
+	 *
+	 * @param  {Number}        keyCode             the key code of the key to use for untab
+	 * @param  {String[]}      [modifierKeyNames]  the modifier key names - valid names are
+	 *                                             'alt', 'ctrl', 'meta', and 'shift'
+	 * @return {String|Function}                   the current untab key combination or the
+	 *                                             tabOverride function
+	 *
+	 * @name untabKey
+	 * @function
+	 * @memberOf jQuery.fn.tabOverride
+	 */
+	tabOverride.untabKey = TABOVERRIDE.untabKey;
 }));
